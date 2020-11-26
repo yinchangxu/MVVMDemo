580a9c5aca9aeb82c9abb90b0e58371279efe75c
yin <179775125@qq.com>
2020-11-26 14:59:15 +0800

    添加demo示例

--- app/src/main/java/com/demo/mvvm/viewmodel/DemoViewModel.java
+++ app/src/main/java/com/demo/mvvm/viewmodel/DemoViewModel.java
@@ -1,9 +1,15 @@
 package com.demo.mvvm.viewmodel;
 
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableBoolean;
 import androidx.databinding.ObservableField;
+import androidx.databinding.ObservableInt;
 import androidx.databinding.ObservableList;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.demo.mvvm.BR;
 import com.demo.mvvm.R;
 import com.demo.mvvm.entity.DemoEntity;
@@ -26,15 +32,41 @@ import me.tatarka.bindingcollectionadapter2.ItemBinding;
  */
 public class DemoViewModel extends BaseViewModel<DemoRepository> {
 
+    //导航栏bean类
     private TitleBarBean titleBarBean = new TitleBarBean();
 
+    //顶部导航栏
     public ObservableField<TitleBarBean> titleBar = new ObservableField<>(titleBarBean);
 
-    //文本
+    //文本文字
     public ObservableField<String> text = new ObservableField<>("啦啦啦");
 
-    //图片
-    public ObservableField<Object> image = new ObservableField<>(R.mipmap.ic_launcher);
+    //文本大小
+    public ObservableInt textSize = new ObservableInt(14);
+
+    //背景
+    public ObservableField<Object> background = new ObservableField<>(R.color._FF7F29);
+
+    //资源图片
+    public ObservableField<Object> image1 = new ObservableField<>(R.mipmap.ic_launcher);
+
+    //网络图片
+    public ObservableField<Object> image2 = new ObservableField<>("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1606381411448&di=ad8e79e78aa1b468829f8641dccade57&imgtype=0&src=http%3A%2F%2F9.pic.9ht.com%2Fup%2F2018-5%2F2018530172841.jpg");
+
+    //按钮点击事件
+    public View.OnClickListener click1 = v -> {
+        click2();
+    };
+
+    public void click2() {
+        ToastUtils.showShort("点击");
+    }
+
+    //单选
+    public ObservableBoolean check1 = new ObservableBoolean(false);
+
+    //多选
+    public ObservableBoolean check2 = new ObservableBoolean(false);
 
     //recycleview
     public final ObservableList<DemoEntity> items = new ObservableArrayList<>();
@@ -42,7 +74,9 @@ public class DemoViewModel extends BaseViewModel<DemoRepository> {
 
     public DemoViewModel(DemoRepository repository) {
         super(repository);
+        //导航栏中心文字
         titleBarBean.tvCenter.set("示例");
+        //导航栏左侧图标点击事件
         titleBarBean.onBack(new TitleBarBean.OnItemEventListener() {
             @Override
             public void onClick() {
@@ -55,6 +89,7 @@ public class DemoViewModel extends BaseViewModel<DemoRepository> {
     protected void onCreate() {
         super.onCreate();
 
+        //假数据
         for (int i = 0; i < 5; i++) {
             DemoEntity demoEntity = new DemoEntity();
             demoEntity.username = String.valueOf(i);
@@ -64,6 +99,7 @@ public class DemoViewModel extends BaseViewModel<DemoRepository> {
             items.add(demoEntity);
         }
 
+        //返回json中data数据为{}
         Map<String, Object> postMap1 = new HashMap<>();
         postMap1.put("username", "123");
         postMap1.put("password", "123");
@@ -79,6 +115,7 @@ public class DemoViewModel extends BaseViewModel<DemoRepository> {
             }
         });
 
+        //返回json中data数据为[]
         Map<String, Object> postMap2 = new HashMap<>();
         postMap2.put("username", "123");
         postMap2.put("password", "123");
